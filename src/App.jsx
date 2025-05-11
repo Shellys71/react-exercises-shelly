6c5609cb6080b1061e7d49973edebac8dee23019
Shelly <shelysaraev@gmail.com>
2025-05-11 18:51:13 +0300

    Added recipe context for global management

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
+
 import "./App.css";
 import AddRecipe from "./components/Recipes/AddRecipe";
 import RecipesList from "./components/Recipes/RecipesList";
-
+import RecipeContext from "./store/recipe-context";
 
 function App() {
   const [recipeList, setRecipeList] = useState([]);
 
   const addRecipeHandler = (recipe) => {
     setRecipeList((prevRecipesList) => {
-      return [...prevRecipesList, {...recipe, id: Math.random().toString()}];
+      return [...prevRecipesList, { ...recipe, id: Math.random().toString() }];
     });
   };
 
   return (
-    <div>
-      <AddRecipe onAddRecipe={addRecipeHandler} />
-      <RecipesList recipes={recipeList} />
-    </div>
+    <RecipeContext.Provider value={{ recipes: recipeList }}>
+      <div>
+        <AddRecipe onAddRecipe={addRecipeHandler} />
+        <RecipesList />
+      </div>
+    </RecipeContext.Provider>
   );
 }
 
--- src/components/Recipes/Recipe.jsx
+++ src/components/Recipes/Recipe.jsx
@@ -8,7 +8,7 @@ const Recipe = (props) => {
           <h1>{props.recipe}</h1>
           <h2>Ingredients:</h2>
           {props.ingredients.map((ingredient, index) => <p key={index}>{ingredient}</p>)}
-          <h2>Instructions</h2>
+          <h2>Instructions:</h2>
           {props.instructions.map((instuction, index) => <p key={index}>{instuction}</p>)}
           <img src={props.src} alt="recipe"/>
         </div>
